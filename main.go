3b1357192bce5da1b32b87e9d3c479707a5f9845
K8sCat <k8scat@gmail.com>
2021-08-18 11:08:22 +0800

    update: remove unused code

--- main.go
+++ main.go
@@ -1,23 +1,12 @@
 package main
 
 import (
-	"flag"
 	"log"
 
-	"github.com/wanhuasong/uniportal/config"
 	"github.com/wanhuasong/uniportal/router"
 )
 
-var cfgFile string
-
 func main() {
 	log.SetFlags(log.Llongfile | log.LstdFlags)
-
-	flag.StringVar(&cfgFile, "c", "./config.json", "Config file")
-	flag.Parse()
-
-	if err := config.InitConfig(cfgFile); err != nil {
-		panic(err)
-	}
 	router.Run()
 }
